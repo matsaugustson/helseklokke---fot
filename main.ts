80452aa54699ae4b362156343530fe8b3904668b
matsaugustson <78408726+matsaugustson@users.noreply.github.com>
2021-12-12 08:49:49 +0100

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,8 @@
 input.onGesture(Gesture.ThreeG, function () {
-    steps += 2
-    basic.showNumber(steps)
-    radio.sendNumber(steps)
+    radio.sendNumber(0)
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.showIcon(IconNames.Diamond)
+    basic.showIcon(IconNames.Square)
 })
-input.onButtonPressed(Button.AB, function () {
-    steps = 0
-    basic.showNumber(steps)
-    radio.sendNumber(steps)
-})
-let steps = 0
 radio.setGroup(1)
-steps = 0
+basic.showIcon(IconNames.Square)
